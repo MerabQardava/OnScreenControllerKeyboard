fa91fd173192ca4f5c04d677d9192c0951ed7dde
MerabQardava <merab.kardava.1@btu.edu.ge>
2024-01-03 03:54:48 +0400

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useGamepads} from "react-gamepads";
+import {useEffect, useState} from "react";
+import StickVisualizer from "./StickVisualizer";
+
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [gamepads, setGamepads] = useState([])
+  useGamepads(_gamepads=>{
+    // console.log(_gamepads)
+    setGamepads(_gamepads[0])
+  })
+  // useEffect(() => {
+  //
+  // }, []);
+  // console.log(typeof (gamepads.axes[1]))
+
+
+
+  return <div>
+    <p>{`left X: ${gamepads.axes[0]}`}</p>
+    <p>{`left Y: ${gamepads.axes[1]}`}</p>
+    <p>{`right X: ${gamepads.axes[2]}`}</p>
+    <p>{`right Y: ${gamepads.axes[3]}`}</p>
+    <StickVisualizer x={gamepads.axes[0]} y={gamepads.axes[1]}/>
+
+
+  </div>;
 }
 
 export default App;
---
+++ src/StickVisualizer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const StickVisualizer = ({ x, y }) => {
+    // Assuming x and y range from -1 to 1
+    const outerCircleRadius = 100;
+    const innerCircleRadius = 20;
+
+    // Calculate the position of the inner circle based on x and y values
+    const innerCircleX = x * outerCircleRadius;
+    const innerCircleY = y * outerCircleRadius;
+
+    return (
+        <div style={{ position: 'relative', width: outerCircleRadius * 2, height: outerCircleRadius * 2, borderRadius: '50%', backgroundColor: '#eee', margin: 'auto' }}>
+            <div
+                style={{
+                    position: 'absolute',
+                    top: '50%',
+                    left: '50%',
+                    transform: `translate(-50%, -50%) translate(${innerCircleX}px, ${innerCircleY}px)`, // Center the inner circle within the outer circle
+                    width: innerCircleRadius * 2,
+                    height: innerCircleRadius * 2,
+                    borderRadius: '50%',
+                    backgroundColor: 'blue',
+                }}
+            />
+        </div>
+    );
+};
+
+export default StickVisualizer;
+
