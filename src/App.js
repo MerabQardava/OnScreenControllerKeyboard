a49e904abdf9ce1455a2d6e6a88f505c0fd9ecd2
MerabQardava <merab.kardava.1@btu.edu.ge>
2024-01-03 04:02:34 +0400

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 
 import './App.css';
-// import {useGamepads} from "react-gamepads";
-// import {useEffect, useState} from "react";
-// import StickVisualizer from "./StickVisualizer";
-//
+import {useGamepads} from "react-gamepads";
+import {useEffect, useState} from "react";
+import StickVisualizer from "./StickVisualizer";
+
 
 
 
 
 function App() {
-  // const [gamepads, setGamepads] = useState([])
-  // useGamepads(_gamepads=>{
-  //   // console.log(_gamepads)
-  //   setGamepads(_gamepads[0])
-  // })
-  // // useEffect(() => {
-  // //
-  // // }, []);
-  // // console.log(typeof (gamepads.axes[1]))
+  const [gamepads, setGamepads] = useState([])
+  useGamepads(_gamepads=>{
+    // console.log(_gamepads)
+    setGamepads(_gamepads[0])
+  })
+  // useEffect(() => {
+  //
+  // }, []);
+  // console.log(typeof (gamepads.axes[1]))
 
 
 
   return <div>
     <p>test</p>
-    {/*<p>{`left X: ${gamepads.axes[0]}`}</p>*/}
-    {/*<p>{`left Y: ${gamepads.axes[1]}`}</p>*/}
-    {/*<p>{`right X: ${gamepads.axes[2]}`}</p>*/}
-    {/*<p>{`right Y: ${gamepads.axes[3]}`}</p>*/}
-    {/*<StickVisualizer x={gamepads.axes[0]} y={gamepads.axes[1]}/>*/}
+    <p>{`left X: ${gamepads.axes[0]}`}</p>
+    <p>{`left Y: ${gamepads.axes[1]}`}</p>
+    <p>{`right X: ${gamepads.axes[2]}`}</p>
+    <p>{`right Y: ${gamepads.axes[3]}`}</p>
+    <StickVisualizer x={gamepads.axes[0]} y={gamepads.axes[1]}/>
 
 
   </div>;
